65369c0ad963e235c1f7d2f2a93df62ff53e50f5
ineslachkhem <lachkhemines70@gmail.com>
2024-05-02 14:33:16 -1000

    partie alarme check

--- server/server.js
+++ server/server.js
@@ -5,18 +5,17 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import alarmeRouter from './routes/api/AirAlarme.js';
-import typealarmeRouter from './routes/api/Air_Typealarme.js';
-import usineRouter from './routes/api/Usine.js';
+
 import entretienRouter from './routes/api/airEntretienRoutes.js';
 //import equipementRouter from './routes/api/airEquipmentRoutes.js';
 import userRouter from './routes/api/user.js';
 import { authRouter } from './routes/api/auth.js';
-import airspvsRouter from './routes/api/airsuperviseur.js';
+//import airspvsRouter from './routes/api/airsuperviseur.js';
 
 dotenv.config();
 
 const app = express();
-const { PORT = 8000, dbpassword } = process.env;
+const { PORT = 5000, dbpassword } = process.env;
 
 // Middleware to log incoming requests
 app.use((req, res, next) => {
@@ -31,11 +30,10 @@ app.use(cors());
 app.use(express.static('./dist/'));
 
 app.use('/api/airalarmes', alarmeRouter);
-app.use('/api/airTypeAlarme', typealarmeRouter);
-app.use('/api/usine', usineRouter);
+
 app.use('/api/entretiens', entretienRouter);
 //app.use('/api/equipements', equipementRouter);
-app.use('/api/airspvs', airspvsRouter);
+//app.use('/api/airspvs', airspvsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 
@@ -44,7 +42,7 @@ app.get('/api', (req, res) => {
     message: 'Welcome to the API',
   });
 });
-const CONNECTION_URL = `mongodb+srv://nejimarwan21:${dbpassword}@piwebcluster.yq3u1v6.mongodb.net`;
+const CONNECTION_URL = `mongodb+srv://ineslachkhem:piwebines@cluster0.kmdgs4p.mongodb.net/`;
 mongoose.connect(CONNECTION_URL, {  })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
