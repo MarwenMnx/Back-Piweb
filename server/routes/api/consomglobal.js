bd4befec6277b31bb7ef538c59a7eebb9eed40bd
Marwen-Neji <78695522+Marwen-Neji@users.noreply.github.com>
2024-05-08 13:27:05 +0100

    fix

--- server/routes/api/consomglobal.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addConsomglobalData } from '../../../controllers/consomglobalController.js';
-
-const consomglobalRouter = express.Router();
-
-// Route to add consomglobal data
-consomglobalRouter.post('/', addConsomglobalData);
-
-export default consomglobalRouter;
--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ import predictionRouter from './routes/api/prediction.js';
 import http from 'http'; // Import the http module
 import { Server } from 'socket.io'; // Import the Server class from socket.io
 import { startSocketLogic } from '../socket.js'; // Import the socket logic function
-import consomglobalRouter from './routes/api/consomglobal.js';
 import airLocalCompresseurRouter from './routes/api/airLocalCompresseur.js'
 
 dotenv.config();
@@ -35,7 +34,7 @@ app.use(express.static('./dist/'));
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/predict', predictionRouter);
-app.use('/api/consomglobal', consomglobalRouter);
+// app.use('/api/consomglobal', consomglobalRouter);
 app.use('/api/airlocalcomp', airLocalCompresseurRouter);
 
 app.get('/api', (req, res) => {
