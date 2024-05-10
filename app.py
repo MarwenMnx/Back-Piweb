0af949ce069893eecdfe7e5db59a6071a5450ca6
SirineZoghlami <sirine.zoghlami@esprit.tn>
2024-05-10 01:42:41 +0100

    fixes

--- app.py
+++ app.py
@@ -1,33 +1,54 @@
 from flask import Flask, request, jsonify
-from joblib import load
+from flask_cors import CORS
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
+CORS(app)
 
-# Load the trained model
-model = load('best_modelefinale123.pkl')
+# Load the saved models
+with open('linear_regression_model_p1.pkl', 'rb') as file1:
+    model_p1 = pickle.load(file1)
 
-# Define API endpoint
+with open('linear_regression_model_p2.pkl', 'rb') as file2:
+    model_p2 = pickle.load(file2)
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    rpm = data['rpm']
-    motor_power = data['motor_power']
-    torque = data['torque']
-    outlet_pressure_bar = data['outlet_pressure_bar']
-    wpump_outlet_press = data['wpump_outlet_press']
-    wpump_power = data['wpump_power']
-    water_flow = data['water_flow']
-    oilpump_power = data['oilpump_power']
+    data = request.get_json()
     
-    # Make prediction
-    input_data = [[rpm, motor_power, torque, outlet_pressure_bar, wpump_outlet_press, wpump_power, water_flow, oilpump_power]]
-    predicted_air_flow = model.predict(input_data)[0]
+    heurefonction = data['heurefonction']
+    temperature = data['temperature']
+    pointderose = data['pointderose']
+    tauxdecharge = data['tauxdecharge']
+    debit = data['debit']
+    production = data['production']
+
+    features = np.array([heurefonction, temperature, pointderose, tauxdecharge, debit, production]).reshape(1, -1)
     
-    
-    # Return prediction and total production as JSON response
-    return jsonify({
-        'debit_de_air_flow': predicted_air_flow
-    })
+    # Scale the features
+    scaler = StandardScaler()
+    with open('scaler_mean.pkl', 'rb') as file:
+        scaler.mean_ = pickle.load(file)
+    with open('scaler_scale.pkl', 'rb') as file:
+        scaler.scale_ = pickle.load(file)
+
+    features_scaled = scaler.transform(features)
+
+    prediction_p1 = model_p1.predict(features_scaled)
+
+    prediction_p2 = model_p2.predict(features_scaled)
+
+    # Adjust pression_1 to be slightly higher than pression_2
+    epsilon = 0.01
+    prediction_p1 += epsilon
+
+    # Format predictions to display only four digits after the decimal point
+    prediction_p1 = round(prediction_p1[0], 4)
+    prediction_p2 = round(prediction_p2[0], 4)
+
+    return jsonify({'pression_1': prediction_p1, 'pression_2': prediction_p2})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='92.168.1.19', port=6000)
+    app.run(debug=True)
